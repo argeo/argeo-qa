19e7d7fc1bdc50fb76dc8f8748c7c599f9e25f5c
Mathieu Baudier <mbaudier@argeo.org>
2020-07-17 08:53:35 +0200

    Make deploy script more robust.

--- doc/deploy-doc.sh
+++ doc/deploy-doc.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+set -e
 
-BASE_DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 WWW_DIR=/srv/www
+ARGEO_DOC=argeo-2.1/doc
 
 # Update
-git -C $BASE_DIR pull
+git -C $DIR pull
 
 # Generate
-./platform/generate-html.sh
-./www/generate-html.sh
+$DIR/platform/generate-html.sh
+$DIR/www/generate-html.sh
 
 # Deploy
-mkdir -p $WWW_DIR/argeo-2.1/doc
-rsync -av --exclude ".*" --exclude "*.sh" --exclude "argeo.css" --exclude "www" $BASE_DIR/ $WWW_DIR/argeo-2.1/doc
-rsync -av $BASE_DIR/theme/argeo.css $WWW_DIR/argeo-2.1/doc
+rm -rf $WWW_DIR/*
+mkdir -p $WWW_DIR/$ARGEO_DOC
+rsync -av --exclude ".*" --exclude "*.sh" --exclude "argeo.css" --exclude "www" $DIR/ $WWW_DIR/$ARGEO_DOC
+rsync -av $DIR/theme/argeo.css $WWW_DIR/$ARGEO_DOC
 
-rsync -av --exclude ".*" --exclude "*.sh" --exclude "argeo.css" $BASE_DIR/www/ $WWW_DIR
-rsync -av $BASE_DIR/theme/argeo.css $WWW_DIR
+rsync -av --exclude ".*" --exclude "*.sh" --exclude "argeo.css" $DIR/www/ $WWW_DIR
+rsync -av $DIR/theme/argeo.css $WWW_DIR
